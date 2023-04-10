f431232deadb02d8be0a6741eabd508310aaab95
leavism <ghuydang@gmail.com>
2023-04-10 12:34:50 -0700

    Styling links and inline code

--- components/PostContent.tsx
+++ components/PostContent.tsx
@@ -19,9 +19,34 @@ export default function PostContent({ post }: { post: Post} ) {
 				className={'rounded-md'}
 				/>
 			),
-			a: (props) => {
-				return <Link href={`${props.href ?? ''}`}>{props.children}</Link>;
-			}
+			a: (props) => (
+				<Link 
+				href={`${props.href ?? ''}`} 
+				className="!no-underline font-semibold relative before:bg-neutral-300 dark:before:bg-neutral-500 before:absolute before:-left-0.5 before:-bottom-0.5 before:w-full before:h-2.5 before:-z-10 before:ease-in-out before:duration-300 before:hover:bottom-0 before:hover:h-full">
+					{props.children}
+				</Link>
+			),
+			h1: (props) => (
+				<h1
+					className="!font-semibold !mb-0"
+				>
+					{props.children}
+				</h1>
+			),
+			h2: (props) => (
+				<h2
+					className="!font-semibold !mb-0 !mt-0"
+				>
+					{props.children}
+				</h2>
+			),
+			code: (props) => (
+				<code
+					className="bg-neutral-200 p-1 rounded dark:bg-neutral-700"
+				>
+					{props.children}
+				</code>
+			)
     }}
   >
     {post.content ?? ''}
