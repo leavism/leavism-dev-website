0c0b9024969a464c49786765824a2d003249d313
leavism <ghuydang@gmail.com>
2023-05-05 13:25:05 -0700

    Abstracted login button into its own component

---
+++ components/Comment/Auth.tsx
@@ -0,0 +1,53 @@
+import { type Session } from 'next-auth';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import Image from 'next/image';
+
+type authButtonProps = {
+  sessionData: Session | null
+  children: React.ReactNode
+}
+
+function AuthButton({
+  sessionData,
+  children,
+}: authButtonProps) {
+  return (
+    <button
+      onClick={sessionData ? () => void signOut() : () => void signIn()}
+      className="flex h-fit justify-center rounded-md border border-solid px-4 py-1 transition-all duration-150 ease-out hover:-translate-x-1 hover:border-gray-700 hover:shadow-[3px_3px_0px] focus-visible:outline-offset-1 dark:border dark:hover:border-white"
+    >
+      {children}
+    </button>
+  )
+}
+
+export default function CommentAuth() {
+  const { data: sessionData } = useSession();
+
+  return (
+    <>
+      {sessionData ? (
+        <div className="flex flex-row items-center justify-end gap-5">
+          {sessionData.user.image ? (
+            <Image
+              src={sessionData.user.image}
+              width={64}
+              height={64}
+              className="!m-0 rounded-full"
+              alt=""
+            />
+          ) : null}
+        <AuthButton sessionData={sessionData}>
+          Sign out
+        </AuthButton>
+        </div>
+      ) : (
+        <div className="flex flex-row items-centers justify-end">
+          <AuthButton sessionData={sessionData}>
+            Sign in to comment
+          </AuthButton>
+        </div>
+      )}
+    </>
+  );
+}
--- components/Comment/index.tsx
+++ components/Comment/index.tsx
@@ -1,42 +1,10 @@
-import Container from "components/Container";
-import { useSession, signIn, signOut } from "next-auth/react";
-import CommentForm from "components/Comment/form";
-import Image from "next/image";
+import CommentAuth from './Auth';
+import CommentForm from './Form';
 
 export default function Comment() {
-  const { data: sessionData } = useSession();
-
   return (
-    <Container>
-      {sessionData ? (
-        <>
-					<div className="flex flex-row items-center justify-end gap-5">
-						{sessionData.user.image ? (
-							<Image
-								src={sessionData.user.image}
-								width={64}
-								height={64}
-								className="rounded-full !m-0"
-								alt=""
-							/>
-						) : null}
-						<button
-							onClick={sessionData ? () => void signOut() : () => void signIn()}
-							className="flex justify-center h-fit rounded-md border border-solid px-4 py-1 transition-all duration-150 ease-out hover:-translate-x-1 hover:border-gray-700 hover:shadow-[3px_3px_0px] focus-visible:outline-offset-1 dark:border dark:hover:border-white"
-						>
-							{"Sign out"}
-						</button>
-					</div>
-          <CommentForm />
-        </>
-      ) : (
-        <button
-          onClick={sessionData ? () => void signOut() : () => void signIn()}
-          className="flex justify-center rounded-full border border-solid border-transparent px-4 py-1 transition-all duration-150 ease-out hover:-translate-x-1 hover:border-gray-700 hover:shadow-[3px_3px_0px] focus-visible:outline-offset-1 dark:border dark:hover:border-white"
-        >
-          {"Sign in to comment"}
-        </button>
-      )}
-    </Container>
+    <>
+      <CommentAuth />
+    </>
   );
 }
