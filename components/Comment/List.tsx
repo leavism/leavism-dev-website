0f024b41d429afb89a75c87a77e617ea657b88dc
leavism <ghuydang@gmail.com>
2023-05-08 13:23:15 -0700

    Stylize comments
    Now shows author name and profile picture. Should be okay for now.

--- components/Comment/List.tsx
+++ components/Comment/List.tsx
@@ -1,16 +1,19 @@
 import { api } from '~/utils/api';
+import Image from 'next/image';
 
 export default function CommentList() {
   const { data: comments } = api.comment.listComment.useQuery();
   return (
     <>
-      {comments?.map((comment) => (
-        <Comment
-          key={comment.id}
-          authorId={comment.authorId}
-          content={comment.content}
-        />
-      ))}
+      <div className="m-1 grid grid-flow-row gap-6">
+        {comments?.map((comment) => (
+          <Comment
+            key={comment.id}
+            authorId={comment.authorId}
+            content={comment.content}
+          />
+        ))}
+      </div>
     </>
   );
 }
@@ -24,8 +27,20 @@ type CommentProps = {
 function Comment({ key: id, authorId, content }: CommentProps) {
   const { data: author } = api.user.getById.useQuery(authorId);
   return (
-    <p key={id}>
-      {author?.name} : {content}
-    </p>
+    <>
+      <div className="flex flex-row items-center gap-5">
+        <Image
+          src={author?.image ?? ''}
+          width={54}
+          height={54}
+          className="!m-0 rounded-full"
+          alt={`${author?.name || 'Someone'}'s profile picture`}
+        />
+        <div className="flex flex-col leading-6">
+          <div className="font-semibold">{author?.name}</div>
+          <div>{content}</div>
+        </div>
+      </div>
+    </>
   );
 }
