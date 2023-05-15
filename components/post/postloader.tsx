0dd741df3cbd5fdede26575c9257aafa88aee2da
leavism <ghuydang@gmail.com>
2023-05-15 14:06:08 -0700

    Posts now display a loading animation
    Posts used to display a 404 when the page was still loading. Now it displays a loading animation.

---
+++ components/post/postloader.tsx
@@ -0,0 +1,39 @@
+import Container from 'components/container';
+import ContentLoader from 'react-content-loader';
+
+const PostLoader = () => (
+  <Container>
+    <ContentLoader viewBox="0 0 50vh 100%" height="100vh" width="100%">
+      <rect x="0" y="0" rx="4" ry="4" width="50%" height="50" />
+      <rect x="0" y="75" rx="4" ry="4" width="100%" height="25" />
+      <rect x="0" y="110" rx="4" ry="4" width="13%" height="25" />
+      <rect x="0" y="150" rx="4" ry="4" width="20%" height="15" />
+
+      <rect x="0" y="190" rx="4" ry="4" width="100%" height="25" />
+
+      <rect x="0" y="240" rx="4" ry="4" width="50%" height="50" />
+      <rect x="0" y="315" rx="4" ry="4" width="100%" height="25" />
+      <rect x="0" y="350" rx="4" ry="4" width="95%" height="25" />
+      <rect x="0" y="385" rx="4" ry="4" width="100%" height="25" />
+      <rect x="0" y="420" rx="4" ry="4" width="80%" height="25" />
+      <rect x="0" y="455" rx="4" ry="4" width="100%" height="25" />
+      <rect x="0" y="490" rx="4" ry="4" width="85%" height="25" />
+
+      <rect x="0" y="540" rx="4" ry="4" width="90%" height="25" />
+      <rect x="0" y="575" rx="4" ry="4" width="100%" height="25" />
+      <rect x="0" y="610" rx="4" ry="4" width="25%" height="25" />
+
+      <rect x="0" y="660" rx="4" ry="4" width="100%" height="25" />
+      <rect x="0" y="695" rx="4" ry="4" width="10%" height="25" />
+
+      <rect x="0" y="745" rx="4" ry="4" width="25%" height="35" />
+      <rect x="0" y="795" rx="4" ry="4" width="100%" height="25" />
+      <rect x="0" y="830" rx="4" ry="4" width="95%" height="25" />
+      <rect x="0" y="865" rx="4" ry="4" width="100%" height="25" />
+      <rect x="0" y="900" rx="4" ry="4" width="100%" height="25" />
+      <rect x="0" y="935" rx="4" ry="4" width="25%" height="25" />
+    </ContentLoader>
+  </Container>
+);
+
+export default PostLoader;
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,52 +1,46 @@
 import { type NextRouter, useRouter } from 'next/router';
 import ErrorPage from 'next/error';
+import distanceToNow from 'lib/util/dateRelative';
+import { api } from '~/utils/api';
 import Comment from 'components/comment';
 import Container from 'components/container';
-import distanceToNow from 'lib/util/dateRelative';
-import PostView from 'components/postview';
-import { api } from '~/utils/api';
+import PostView from 'components/post/postview';
+import PostLoader from 'components/post/postloader';
 
 export default function PostPage() {
   const router: NextRouter = useRouter();
   const postSlug = api.post.getPostBySlug;
-  const { data: post } = postSlug.useQuery({
+  const { data: post, isLoading } = postSlug.useQuery({
     slug: router.query.slug as string,
   });
 
-  if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />;
-  }
-
+  if (isLoading) return <PostLoader />;
   if (!post) return <ErrorPage statusCode={404} />;
 
   return (
     <Container>
-      {router.isFallback ? (
-        <div>Loading…</div>
-      ) : (
-        <div>
-          <article>
-            <header className="prose-base prose-zinc sm:prose-lg md:prose-xl lg:prose-2xl">
-              <h1 className="!mb-0 font-bold">{post?.title}</h1>
-              {post?.description ? (
-                <p className="!my-2">{post?.description}</p>
-              ) : null}
+      <div>
+        <article>
+          <header className="prose-base prose-zinc sm:prose-lg md:prose-xl lg:prose-2xl">
+            <h1 className="!mb-0 font-bold">{post?.title}</h1>
+            {post?.description ? (
+              <p className="!my-2">{post?.description}</p>
+            ) : null}
 
-              {post?.createdAt ? (
-                <time className="!mt-2 flex text-base text-gray-400">
-                  {distanceToNow(new Date(post?.createdAt))}
-                </time>
-              ) : null}
-            </header>
+            {post?.createdAt ? (
+              <time className="!mt-2 flex text-base text-gray-400">
+                {distanceToNow(new Date(post?.createdAt))}
+              </time>
+            ) : null}
+          </header>
 
-            <section className="prose-base prose-neutral mt-10 sm:prose-base md:prose-lg lg:prose-lg prose-a:underline">
-              <PostView post={post} />
-            </section>
-          </article>
+          <section className="prose-base prose-neutral mt-10 sm:prose-base md:prose-lg lg:prose-lg prose-a:underline">
+            <PostView post={post} />
+          </section>
+        </article>
 
-          <Comment />
-        </div>
-      )}
+        <Comment />
+      </div>
     </Container>
   );
 }
