a09281aca8f4708240429b73d3f2774fc9f2bbec
Huy Dang <ghuydang@gmail.com>
2023-05-18 09:25:48 -0700

    Removed unecessary empty tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,17 @@ import Container from 'components/Container';
 
 const Home: NextPage = () => {
   return (
-    <>
-      <Container>
-        <div className="space-y-6">
-          <h1 className="font-bold">
-            Hey! My name is <strong>Huy Dang</strong>.
-          </h1>
-          <p>
-            I&apos;m an undergraduate student at SFSU trying to learn web
-            development.
-          </p>
-        </div>
-      </Container>
-    </>
+    <Container>
+      <div className="space-y-6">
+        <h1 className="font-bold">
+          Hey! My name is <strong>Huy Dang</strong>.
+        </h1>
+        <p>
+          I&apos;m an undergraduate student at SFSU trying to learn web
+          development.
+        </p>
+      </div>
+    </Container>
   );
 };
 
