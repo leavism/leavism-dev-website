d08c9c135d1c50454a53b3d65ce58b6593048b6a
Huy Dang <ghuydang@gmail.com>
2023-05-19 09:11:49 -0700

    await the user Prisma function call

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -2,8 +2,8 @@ import { z } from 'zod';
 import { createTRPCRouter, publicProcedure } from '~/server/api/trpc';
 
 export const userRouter = createTRPCRouter({
-  getById: publicProcedure.input(z.string()).query(({ ctx, input }) => {
-    return ctx.prisma.user.findFirst({
+  getById: publicProcedure.input(z.string()).query(async ({ ctx, input }) => {
+    return await ctx.prisma.user.findFirst({
       where: {
         id: input,
       },
