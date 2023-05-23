1304b4dcbe7cf28214e690195be5979205dd2029
Huy Dang <ghuydang@gmail.com>
2023-05-22 23:03:20 -0700

    Implement basic new post form

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -3,7 +3,7 @@ import AdminAuth from 'components/Admin/AdminAuth';
 import Container from 'components/Container';
 import { type NextPage } from 'next';
 import Link from 'next/link';
-import Router from 'next/router';
+import Router, { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { api } from '~/utils/api';
 import { useUserRole } from '~/utils/hooks';
@@ -11,10 +11,12 @@ import { useUserRole } from '~/utils/hooks';
 const AdminPage: NextPage = () => {
   const { admin } = useUserRole();
   const { data: allBlogs } = api.blog.listBlog.useQuery();
-  const blogMutation = api.blog.editBlogContent.useMutation();
+  const blogMutation = api.blog.deleteBlog.useMutation();
+  const router = useRouter();
 
   const handleDelete = (slug: string) => {
-    blogMutation.mutate({ slug, content: 'test' });
+    blogMutation.mutate({ slug });
+    router.reload();
   };
 
   useEffect(() => {
@@ -25,37 +27,45 @@ const AdminPage: NextPage = () => {
 
   return (
     <Container>
-      <AdminAuth />
-      <div className="mt-8">
-        {allBlogs?.length ? (
-          allBlogs.map((blog) => (
-            <article key={blog.slug} className="mb-10">
-              {blog.slug ? (
-                <Link href={`/blog/${blog.slug}`} className="font-bold">
-                  <h1 className="!my-0">{blog.title}</h1>
-                </Link>
-              ) : null}
-              <p className="!my-1">{blog.description}</p>
-              <div className="flex flex-row gap-5">
+      <div className="flex flex-col gap-10">
+        <AdminAuth />
+        <Link
+          href="/admin/newpost"
+          className="flex h-fit w-full justify-center rounded-md border border-solid px-4 py-2 text-2xl font-bold transition-all duration-150 ease-out hover:-translate-x-1 hover:border-gray-700 hover:shadow-[3px_3px_0px] focus-visible:outline-offset-1 dark:border dark:hover:border-white"
+        >
+          New Post
+        </Link>
+        <div className="mt-8">
+          {allBlogs?.length ? (
+            allBlogs.map((blog) => (
+              <article key={blog.slug} className="mb-10">
                 {blog.slug ? (
-                  <>
-                    <BlogEditButton path={`/admin/edit/${blog.slug}`}>
-                      Edit
-                    </BlogEditButton>
-                    <button
-                      onClick={() => handleDelete(blog.slug)}
-                      className="flex h-fit justify-center rounded-md border border-solid px-4 py-1 transition-all duration-150 ease-out hover:-translate-x-1 hover:border-gray-700 hover:shadow-[3px_3px_0px] focus-visible:outline-offset-1 dark:border dark:hover:border-white"
-                    >
-                      Delete
-                    </button>
-                  </>
+                  <Link href={`/blog/${blog.slug}`} className="font-bold">
+                    <h1 className="!my-0">{blog.title}</h1>
+                  </Link>
                 ) : null}
-              </div>
-            </article>
-          ))
-        ) : (
-          <p>No blog posted yet 😐</p>
-        )}
+                <p className="!my-1">{blog.description}</p>
+                <div className="flex flex-row gap-5">
+                  {blog.slug ? (
+                    <>
+                      <BlogEditButton path={`/admin/edit/${blog.slug}`}>
+                        Edit
+                      </BlogEditButton>
+                      <button
+                        onClick={() => handleDelete(blog.slug)}
+                        className="flex h-fit justify-center rounded-md border border-solid px-4 py-1 transition-all duration-150 ease-out hover:-translate-x-1 hover:border-gray-700 hover:shadow-[3px_3px_0px] focus-visible:outline-offset-1 dark:border dark:hover:border-white"
+                      >
+                        Delete
+                      </button>
+                    </>
+                  ) : null}
+                </div>
+              </article>
+            ))
+          ) : (
+            <p>No blog posted yet 😐</p>
+          )}
+        </div>
       </div>
     </Container>
   );
---
+++ src/pages/admin/newpost.tsx
@@ -0,0 +1,121 @@
+import Container from 'components/Container';
+import { useSession } from 'next-auth/react';
+import Router, { useRouter } from 'next/router';
+import { useReducer, type FormEvent, useEffect } from 'react';
+import { api } from '~/utils/api';
+import { useUserRole } from '~/utils/hooks';
+
+export default function NewPost() {
+  const descriptionReducer = (
+    state: { description?: string },
+    event: { target: { name: string; value: string } }
+  ) => {
+    return {
+      ...state,
+      [event.target.name]: event.target.value,
+    };
+  };
+
+  const contentReducer = (
+    state: { content?: string },
+    event: { target: { name: string; value: string } }
+  ) => {
+    return {
+      ...state,
+      [event.target.name]: event.target.value,
+    };
+  };
+
+  const titleReducer = (
+    state: { title?: string },
+    event: { target: { name: string; value: string } }
+  ) => {
+    return {
+      ...state,
+      [event.target.name]: event.target.value,
+    };
+  };
+
+  const [descriptionData, setDescriptionData] = useReducer(
+    descriptionReducer,
+    {}
+  );
+  const [contentData, setContentData] = useReducer(contentReducer, {});
+  const [titleData, setTitleData] = useReducer(titleReducer, {});
+
+  const router = useRouter();
+  const { data: sessionData } = useSession();
+  const { admin } = useUserRole();
+
+  useEffect(() => {
+    if (!admin) {
+      void Router.push('/enter');
+    }
+  }, [admin]);
+
+  const postBlog = api.blog.postBlog.useMutation();
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!titleData.title) return;
+    if (!contentData.content) return;
+    if (!descriptionData.description) return;
+    if (!sessionData?.user.id) return;
+
+    const newBlogPost = {
+      title: titleData.title,
+      slug: titleData.title?.replaceAll(' ', '-').toLowerCase(),
+      authorId: sessionData?.user.id,
+      content: contentData.content,
+      description: descriptionData.description,
+    };
+
+    postBlog.mutate(newBlogPost);
+    await router.push(
+      `/blog/${titleData.title?.replaceAll(' ', '-').toLowerCase() ?? ''}`
+    );
+  };
+
+  return (
+    <Container>
+      <div>
+        <form
+          className="flex flex-col"
+          onSubmit={(event) => void handleSubmit(event)}
+        >
+          <label>Title</label>
+          <input
+            className="m-3 w-full rounded-lg border-2 border-solid border-neutral-200 p-3 outline-none focus:border-2 focus:border-neutral-400 dark:bg-neutral-600"
+            onChange={setTitleData}
+            name="title"
+            type="text"
+            required
+            minLength={1}
+          ></input>
+          <label>Description</label>
+          <textarea
+            className="m-3 w-full rounded-lg border-2 border-solid border-neutral-200 p-3 outline-none focus:border-2 focus:border-neutral-400 dark:bg-neutral-600"
+            rows={2}
+            onChange={setDescriptionData}
+            name="description"
+            required
+            minLength={1}
+          ></textarea>
+          <label>Content</label>
+          <textarea
+            className="m-3 w-full rounded-lg border-2 border-solid border-neutral-200 p-3 outline-none focus:border-2 focus:border-neutral-400 dark:bg-neutral-600"
+            rows={20}
+            onChange={setContentData}
+            name="content"
+            required
+            minLength={1}
+          ></textarea>
+          <input
+            className="flex h-fit w-fit justify-center self-end rounded-md border border-solid px-4 py-1 transition-all duration-150 ease-out hover:-translate-x-1 hover:border-gray-700 hover:shadow-[3px_3px_0px] focus-visible:outline-offset-1 dark:border dark:hover:border-white"
+            type="submit"
+          ></input>
+        </form>
+      </div>
+    </Container>
+  );
+}
