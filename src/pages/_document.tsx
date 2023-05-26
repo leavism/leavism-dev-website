fe71123a4c68ad8ae741765d7494252c61a58738
Huy Dang <ghuydang@gmail.com>
2023-05-25 17:56:22 -0700

    Hardcoded some metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,15 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta name="robots" content="follow, index" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta property="og:title" content="Huy's Website" />
+        <meta
+          property="og:description"
+          content="It's my own personal website! Follow me as if my life was interesting!"
+        />
+        <meta
+          property="og:image"
+          content="https://avatars.githubusercontent.com/u/1857296?v=4"
+        />
       </Head>
       <body className="prose-base flex min-h-screen flex-col bg-white text-gray-700 antialiased sm:prose-base md:prose-lg lg:prose-xl dark:bg-neutral-800 dark:text-white">
         <Main />
